45a84b952bc8efd06eba1f0cc428ff2ae15b6160
Darkhan Akhmetov <63459203+darkhanakh@users.noreply.github.com>
2026-06-10 22:31:17 +0500

    feat: add buffer to batch syscalls and one lock to optimize perf

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,27 @@
-use std::env;
+use std::io::{BufWriter, Write};
+use std::{env, io, process};
 
-fn main() {
+fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
-        println!("Usage: fastbat <filename>");
-        return;
+        eprintln!("Usage: fastbat <filename>");
+        process::exit(1);
     }
     let filename = &args[1];
+
+    let mut stream = BufWriter::new(io::stdout().lock());
+
     match std::fs::read_to_string(filename) {
         Ok(contents) => {
             for (line_number, line) in contents.lines().enumerate() {
-                println!("{:>4}: {}", line_number + 1, line);
+                writeln!(stream, "{:>4}: {}", line_number + 1, line)?;
             }
+            stream.flush()?;
+            Ok(())
         }
         Err(e) => {
             eprintln!("fastbat: {}: {}", filename, e);
-            std::process::exit(1);
+            Err(e)
         }
     }
 }
