1714138381521808fd4eadcff72abe15d6d054fa
Darkhan Akhmetov <63459203+darkhanakh@users.noreply.github.com>
2026-06-10 22:00:22 +0500

    feat: add simple file content to stdout

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("Usage: fastbat <filename>");
+        return;
+    }
+    let filename = &args[1];
+    match std::fs::read_to_string(filename) {
+        Ok(contents) => {
+            for line in contents.lines() {
+                println!("{}", line);
+            }
+        }
+        Err(e) => {
+            eprintln!("fastbat: {}: {}", filename, e);
+            std::process::exit(1);
+        }
+    }
 }
